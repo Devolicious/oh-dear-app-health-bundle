b12446fd001a95ba219b91596d0b2e0f80bccb62
Ruud Denivel <ruud@devolution.be>
2023-11-15 21:35:42 +0100

    update phpunit config + add dev dependency + add checker test

--- Tests/Unit/Checker/DatabaseCheckerTest.php
+++ Tests/Unit/Checker/DatabaseCheckerTest.php
@@ -6,17 +6,26 @@ namespace Devolicious\OhDearAppHealthBundle\Tests\Unit\Checker;
 
 use Devolicious\OhDearAppHealthBundle\Checker\CheckerInterface;
 use Devolicious\OhDearAppHealthBundle\Checker\DatabaseChecker;
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use OhDear\HealthCheckResults\CheckResult;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 final class DatabaseCheckerTest extends TestCase
 {
+    /** @var MockObject<EntityManagerInterface> */
+    /** @phpstan-ignore-next-line */
+    private MockObject $entityManager;
+
     /** @phpstan-ignore-next-line */
     private CheckerInterface $checker;
 
     protected function setUp(): void
     {
-        //        $this->checker = new DatabaseChecker($this->createMock(EntityManagerInterface::class));
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->checker = new DatabaseChecker($this->entityManager);
 
         parent::setUp();
     }
@@ -26,8 +35,7 @@ final class DatabaseCheckerTest extends TestCase
      */
     public function frequency_should_be(): void
     {
-        $this->markTestSkipped('Not implemented yet');
-        //        $this->assertEquals(60, $this->checker->frequency());
+        $this->assertEquals(0, $this->checker->frequency());
     }
 
     /**
@@ -35,22 +43,71 @@ final class DatabaseCheckerTest extends TestCase
      */
     public function run_check_success(): void
     {
-        $this->markTestSkipped('Not implemented yet');
+        $connection = $this->createMock(Connection::class);
+        $connection
+            ->expects($this->once())
+            ->method('connect')
+            ->willReturn(true);
 
-        //        $result = $this->checker->runCheck();
-        //        $this->assertEquals($this->checker->identify(), $result->name);
-        //        $this->assertEquals(CheckResult::STATUS_OK, $result->status);
+        $connection
+            ->expects($this->once())
+            ->method('isConnected')
+            ->willReturn(true);
+
+        $this->entityManager
+            ->expects($this->exactly(2))
+            ->method('getConnection')
+            ->willReturn($connection);
+
+        $result = $this->checker->runCheck();
+        $this->assertEquals($this->checker->identify(), $result->name);
+        $this->assertEquals(CheckResult::STATUS_OK, $result->status);
     }
 
     /**
      * @test
      */
-    public function run_check_failed(): void
+    public function run_check_failed_not_connected(): void
     {
-        $this->markTestSkipped('Not implemented yet');
+        $connection = $this->createMock(Connection::class);
+        $connection
+            ->expects($this->once())
+            ->method('connect')
+            ->willReturn(true);
 
-        //        $result = $this->checker->runCheck();
-        //        $this->assertEquals($this->checker->identify(), $result->name);
-        //        $this->assertEquals(CheckResult::STATUS_FAILED, $result->status);
+        $connection
+            ->expects($this->once())
+            ->method('isConnected')
+            ->willReturn(false);
+
+        $this->entityManager
+            ->expects($this->exactly(2))
+            ->method('getConnection')
+            ->willReturn($connection);
+
+        $result = $this->checker->runCheck();
+        $this->assertEquals($this->checker->identify(), $result->name);
+        $this->assertEquals(CheckResult::STATUS_FAILED, $result->status);
+    }
+
+    /**
+     * @test
+     */
+    public function run_check_failed_with_exception(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $connection
+            ->expects($this->once())
+            ->method('connect')
+            ->willThrowException(new Exception());
+
+        $this->entityManager
+            ->expects($this->once())
+            ->method('getConnection')
+            ->willReturn($connection);
+
+        $result = $this->checker->runCheck();
+        $this->assertEquals($this->checker->identify(), $result->name);
+        $this->assertEquals(CheckResult::STATUS_FAILED, $result->status);
     }
 }
