d494568fc4726e79da696f209748a8efdc0fce7c
Ruud Denivel <ruud@devolution.be>
2023-11-15 10:51:02 +0100

    Remove unnecessary method

--- Store/ResultStore.php
+++ Store/ResultStore.php
@@ -7,8 +7,5 @@ interface ResultStore
 {
     public function save(string $identifier, StoredResult $result): void;
 
-//    public function get(string $key): array;
-
     public function fetchLastResult(string $identifier): ?StoredResult;
-
 }
